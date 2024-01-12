c3482b621db03c6d2ef078055a674de4e572e79f
Ishmum <ishmum20000@gmail.com>
2024-01-12 23:12:01 +0600

    Update conditions on viewing cart items

--- components/cart/cart/Cart.jsx
+++ components/cart/cart/Cart.jsx
@@ -4,6 +4,7 @@ import { useRouter } from 'expo-router';
 import styles from '../../home/nearby/nearbyjobs.style';
 import { ActivityIndicator } from 'react-native';
 import { COLORS } from '../../../constants';
+import CartItem from '../cartitem/CartItem';
 
 const Cart = () => {
   const router = useRouter();
@@ -16,7 +17,7 @@ const Cart = () => {
         <ActivityIndicator size={'large'} color={COLORS.primary} />
       ) : error ? (
         <Text>Something went wrong</Text>
-      ) : ()}
+      ) : (<CartItem />)}
     </View>
   )
 }
